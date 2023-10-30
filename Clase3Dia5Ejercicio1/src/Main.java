e8deebe4431c0728626092c38d3c256eb8bc4ea9
Oscar2912 <oscar.cuencamartin@gmail.com>
2023-10-30 11:46:26 +0100

    Ejercicio grupal verduras

---
+++ Clase3Dia5Ejercicio1/src/Main.java
@@ -0,0 +1,54 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Crear un vector (array) de tipo Verdura que pueda almacenar 5 verduras. Crear 5 verduras y guardarlas en el vector.
+        Verdura[] verduras = new Verdura[5];
+
+        verduras[0] = new Verdura(1, "Lechuga", "Verde", 10, "No");
+        verduras[1] = new Verdura(2, "Acelgas", "Verde", 15, "Si");
+        verduras[2] = new Verdura(3, "Guisantes", "Verde", 2, "No");
+        verduras[3] = new Verdura(4, "Champiñones", "Marrón", 20, "Si");
+        verduras[4] = new Verdura(5, "Cebolla", "Blanco", 15, "No");
+
+        //Recorrer el vector creado y mostrar por pantalla el nombre y las calorías de las verduras almacenadas.
+        for (int i = 0; i < verduras.length; i++){
+            System.out.println("---- Información verdura " + (i+1) + " ----");
+            System.out.println("Nombre: " + verduras[i].getNombre());
+            System.out.println("Calorías: " + verduras[i].getCalorias());
+        }
+
+        //Cambiar todos los datos de dos verduras. Mostrar por pantalla los datos de todas las verduras luego del cambio.
+        System.out.println("---- Realizando cambio de datos sobre 2 verduras ----");
+        verduras[2].setId(6);
+        verduras[2].setNombre("Remolacha");
+        verduras[2].setColor("Morado");
+        verduras[2].setCalorias(8);
+        verduras[2].setDebeCocinarse("Si");
+
+        System.out.println("...");
+
+        verduras[4].setId(7);
+        verduras[4].setNombre("Zanahoria");
+        verduras[4].setColor("Naranja");
+        verduras[4].setCalorias(12);
+        verduras[4].setDebeCocinarse("No");
+
+        System.out.println("---- Información de las verduras tras el cambio de datos ----");
+        for (Verdura nuevasVerduras : verduras){
+            System.out.println("Información Verdura " + nuevasVerduras.getId());
+            System.out.println("Id: " + nuevasVerduras.getId());
+            System.out.println("Nombre: " + nuevasVerduras.getNombre());
+            System.out.println("Color: " + nuevasVerduras.getColor());
+            System.out.println("Calorías: " + nuevasVerduras.getCalorias());
+            System.out.println("¿Debe cocinarse?: " + nuevasVerduras.getDebeCocinarse());
+        }
+
+        //Recorrer el vector y mostrar por pantalla únicamente a las verduras que sean de color verde.
+        System.out.println("---- Verduras de color verde ----");
+        for (Verdura verdurasVerdes : verduras){
+            if(verdurasVerdes.getColor().equals("Verde"))
+                System.out.println(verdurasVerdes.getNombre());
+        }
+
+    }
+}
---
+++ Clase3Dia5Ejercicio1/src/Verdura.java
@@ -0,0 +1,69 @@
+public class Verdura {
+    private int id;
+    private String nombre;
+    private String color;
+    private int calorias;
+    private String debeCocinarse;
+
+    public Verdura() {
+    }
+
+    public Verdura(int id, String nombre, String color, int calorias, String debeCocinarse) {
+        this.id = id;
+        this.nombre = nombre;
+        this.color = color;
+        this.calorias = calorias;
+        this.debeCocinarse = debeCocinarse;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getCalorias() {
+        return calorias;
+    }
+
+    public void setCalorias(int calorias) {
+        this.calorias = calorias;
+    }
+
+    public String getDebeCocinarse() {
+        return debeCocinarse;
+    }
+
+    public void setDebeCocinarse(String debeCocinarse) {
+        this.debeCocinarse = debeCocinarse;
+    }
+
+    @Override
+    public String toString() {
+        return "Verdura{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", color='" + color + '\'' +
+                ", calorias=" + calorias +
+                ", debeCocinarse='" + debeCocinarse + '\'' +
+                '}';
+    }
+}
